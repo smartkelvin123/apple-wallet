6edbe64372679c2b940f992a369b6cbe8589c094
unknown <smartkelvin66@gmail.com>
2024-04-07 10:44:58 +0100

    smart

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, SafeAreaView } from "react-native";
+import CardsList from "./src/component/CardLists";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+
+export default function App() {
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <CardsList />
+
+        <StatusBar style="light" />
+      </SafeAreaView>
+    </GestureHandlerRootView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+  },
+});
---
+++ src/component/CardLists.tsx
@@ -0,0 +1,67 @@
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import { View, useWindowDimensions } from "react-native";
+import {
+  cancelAnimation,
+  useSharedValue,
+  withDecay,
+  clamp,
+  withClamp,
+} from "react-native-reanimated";
+import Card from "./cards";
+import { useState } from "react";
+
+const cards = [
+  require("../../assets/cards/Card 1.png"),
+  require("../../assets/cards/Card 2.png"),
+  require("../../assets/cards/Card 3.png"),
+  require("../../assets/cards/Card 4.png"),
+  require("../../assets/cards/Card 5.png"),
+  require("../../assets/cards/Card 6.png"),
+  require("../../assets/cards/Card 7.png"),
+  require("../../assets/cards/Card 8.png"),
+  require("../../assets/cards/Card 9.png"),
+];
+const CardsList = () => {
+  const [listHeight, setListHeight] = useState(0);
+  const { height: screenHeight } = useWindowDimensions();
+  const activeCardIndex = useSharedValue(null);
+
+  const scrollY = useSharedValue(0);
+  const maxScrollY = listHeight - screenHeight + 70;
+  const pan = Gesture.Pan()
+    .onBegin(() => {
+      cancelAnimation(scrollY);
+    })
+    .onStart(() => {
+      console.log("Panning started");
+    })
+    .onChange((event) => {
+      scrollY.value = clamp(scrollY.value - event.changeY, 0, maxScrollY);
+    })
+    .onEnd((event) => {
+      scrollY.value = withClamp(
+        { min: 0, max: maxScrollY },
+        withDecay({ velocity: -event.velocityY })
+      );
+    });
+  return (
+    <GestureDetector gesture={pan}>
+      <View
+        style={{ padding: 10 }}
+        onLayout={(event) => setListHeight(event.nativeEvent.layout.height)}
+      >
+        {cards.map((card, index) => (
+          <Card
+            key={index}
+            card={card}
+            index={index}
+            scrollY={scrollY}
+            activeCardIndex={activeCardIndex}
+          />
+        ))}
+      </View>
+    </GestureDetector>
+  );
+};
+
+export default CardsList;
